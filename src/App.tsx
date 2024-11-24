f7d1b7cc0c1ed76c3ad8fcbfaa2a5a5747a66c12
hsgw <urkwtky@gmail.com>
2024-11-25 02:47:08 +0900

    add description and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,52 @@
-import { useState } from "react";
 import styles from "./App.module.css";
 import Artsey from "./components/Artsey";
 
 function App() {
   return (
-    <>
-      <h1>Artsey Simulator</h1>
-      <Artsey />
-    </>
+    <div className={styles.root}>
+      <header>
+        <h1 className={styles.title}>Artsey Simulator</h1>
+        <div className={styles.description}>
+          <p>
+            これは手元のキーボードでArtseyキーマップを試すためのシミュレータです。
+          </p>
+          <p>
+            Artseyキーマップのうち、
+            <b>アルファベット+スペース+バックスペース</b>が入力できます。
+            <br />
+            練習モードでは次の文字のキーマップを表示しながら入力します。
+          </p>
+          <p>
+            Artseyキーマップについて書いた記事は<a href="#">こちら</a>。
+          </p>
+        </div>
+      </header>
+      <main>
+        <Artsey />
+      </main>
+      <footer>
+        <h3>ソースコード</h3>
+        <ul>
+          <li>
+            <a href="#">Github</a>
+            <br />
+            ライセンスについてはgithubリポジトリを参照してください。
+          </li>
+        </ul>
+        <h3>参考にしたサイト</h3>
+        <ul>
+          <li>
+            <a href="http://artsey.io">http://artsey.io</a>
+          </li>
+          <li>
+            <a href="https://artseyio.github.io/artseyio-tester/">
+              https://artseyio.github.io/artseyio-tester/
+            </a>
+          </li>
+        </ul>
+        <div className={styles.copyright}>© 2024 5z6p Instruments, hsgw</div>
+      </footer>
+    </div>
   );
 }
 
