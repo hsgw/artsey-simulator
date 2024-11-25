f9ea41d7ccc8f20ff74dfab9b9f84637255cc574
hsgw <urkwtky@gmail.com>
2024-11-26 02:11:46 +0900

    styling confing

--- src/components/ComboConfig.tsx
+++ src/components/ComboConfig.tsx
@@ -1,6 +1,8 @@
 import { Layout } from "../utils/artsey";
 import { Config } from "../utils/config";
 
+import styles from "./ComboConfig.module.css";
+
 type Props = {
   config: Config;
   onKeyBindChange: (keyBind: Config["keyBind"]) => void;
@@ -16,55 +18,53 @@ export default function ComboConfig({
 }: Props) {
   return (
     <div>
+      <h2 className={styles.title}>Config</h2>
       <div>
-        <h2>Config</h2>
-        <div>
-          <h3>Key Config</h3>
-          <div>
-            {Layout.map((row, i) => (
-              <div key={`keybind-row-${i}`}>
-                {row.map((keycode, j) => (
-                  <label key={`keybind-col-${j}`}>
-                    {`${keycode}: `}
-                    <input
-                      type="text"
-                      readOnly
-                      value={keyBind[keycode]}
-                      onKeyDown={(e) => {
-                        const newKeyBind = {
-                          ...keyBind,
-                          [keycode]: e.key,
-                        };
-                        onKeyBindChange(newKeyBind);
-                      }}
-                    />
-                  </label>
-                ))}
-              </div>
-            ))}
-          </div>
+        <h3>Keybind</h3>
+        <div className={[styles.keybind, styles.inner].join(" ")}>
+          {Layout.map((row, i) => (
+            <div key={`keybind-row-${i}`} className={styles.row}>
+              {row.map((keycode, j) => (
+                <div key={`keybind-col-${j}`} className={styles.col}>
+                  <div className={styles.key}>{keycode}</div>
+                  <input
+                    type="text"
+                    readOnly
+                    value={keyBind[keycode]}
+                    onKeyDown={(e) => {
+                      const newKeyBind = {
+                        ...keyBind,
+                        [keycode]: e.key,
+                      };
+                      onKeyBindChange(newKeyBind);
+                    }}
+                  />
+                </div>
+              ))}
+            </div>
+          ))}
         </div>
-        <div>
-          <h3>Combo Timeout</h3>
-          <label>
-            milliseconds:{" "}
-            <input
-              type="number"
-              value={comboTimeout}
-              onChange={(e) => onComboTimeoutChange(Number(e.target.value))}
-            />
-          </label>
+      </div>
+      <div>
+        <h3>Combo Timeout</h3>
+        <div className={[styles.inner, styles.comboTimeout].join(" ")}>
+          <input
+            type="number"
+            value={comboTimeout}
+            onChange={(e) => onComboTimeoutChange(Number(e.target.value))}
+          />
+          <div className={styles.label}>milliseconds</div>
         </div>
-        <div>
-          <h3>Word Count</h3>
-          <label>
-            words:{" "}
-            <input
-              type="number"
-              value={wordCount}
-              onChange={(e) => onWordCountChange(Number(e.target.value))}
-            />
-          </label>
+      </div>
+      <div>
+        <h3>Word Count</h3>
+        <div className={[styles.inner, styles.wordCount].join(" ")}>
+          <input
+            type="number"
+            value={wordCount}
+            onChange={(e) => onWordCountChange(Number(e.target.value))}
+          />
+          <div className={styles.label}>words</div>
         </div>
       </div>
     </div>
